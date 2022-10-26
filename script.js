b34feeb96f4b3f2adb93b08b436f407861db0d08
hammzyy <okenlaha@gmail.com>
2022-10-26 13:47:03 +0100

    updated functionality

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+
+const val = document.querySelector("#val");
+
+const input = document.querySelector("input")
+
+
 let num= 0;
-
-const value = document.querySelector("#val");
-const plus = document.querySelector("#increase");
-const minus = document.querySelector("#decrease");
-const reset = document.querySelector("#reset")
-const input = document.innerHTML("input")
-
-
 
 
 
 function add(){
-    if (input.value.length != 0) {
+    if (input.num.length != 0) {
         num += Math.abs(Number(input.num))
         val.innerHTML = num
     }
